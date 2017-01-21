5b401c8cce0ce7b72c04e92e3c0e84bed72a4d07
linted <linted90@hotmail.com>
2017-01-21 17:12:22 -0500

    created basic view for mapgen

--- MapTheEarth/mapgen/views.py
+++ MapTheEarth/mapgen/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Site under construction. This is the future page for mapgen.")
