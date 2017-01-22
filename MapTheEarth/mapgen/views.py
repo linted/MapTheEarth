a7916f0d7829f65271c6a0e707ad700b149cbc27
linted <linted90@hotmail.com>
2017-01-21 21:39:27 -0500

    removed uneeded apps, and added svg view

--- MapTheEarth/mapgen/views.py
+++ MapTheEarth/mapgen/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Site under construction. This is the future page for mapgen.")
+
+def get_svg(request, svg_id):
+    return HttpResponse("Site under construction. congratz on getting here though!")
